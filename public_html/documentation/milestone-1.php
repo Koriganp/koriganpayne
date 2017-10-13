dc85d20b5a0df40610c7bf59ce2cd49de356237f
Korigan Payne <koriganp@gmail.com>
2017-10-13 09:53:52 -0600

    Added content to first section of page

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,6 +3,20 @@
 	<head>
 		<meta charset="utf-8"/>
 		<meta name="author" content="Korigan Payne"/>
-		<meta name="description" content=""
+		<meta name="description" content="direction of the personal website project"/>
+		<title>Milestone 1</title>
 	</head>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<h2>Purpose</h2>
+		<p>To present myself in a competent and professional manner to prospective clients and employers.</p>
+		<h2>Audience</h2>
+		<p>The intended audience of this site is for employment reasons, either to give tangible evidence of my work to
+			prospective clients or let employers see the quality/content of my code.</p>
+		<h2>Goal</h2>
+		<p>To attract potential clients and employers to myself and my work.</p>
+		<h1>Persona</h1>
+
+
+	</body>
 </html>
