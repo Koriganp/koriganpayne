a6e5c817938829755fc64d16a4f7d3142eb18c6b
Korigan Payne <koriganp@gmail.com>
2017-10-13 12:22:45 -0600

    Added style directory and css

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,21 +4,26 @@
 		<meta charset="utf-8"/>
 		<meta name="author" content="Korigan Payne"/>
 		<meta name="description" content="direction of the personal website project"/>
+		<link href="../assets/styles/milestone-1.css" rel="stylesheet" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
 		<title>Milestone 1</title>
 	</head>
 	<body>
-		<h1>Purpose, Audience, Goal</h1>
-		<h2>Purpose</h2>
-		<p>To present myself in a competent and professional manner to prospective clients and employers.</p>
-		<h2>Audience</h2>
-		<p>The intended audience of this site is for employment reasons, either to give tangible evidence of my work to
+		<main>
+			<h1>Purpose, Audience, Goal</h1>
+			<h2>Purpose</h2>
+			<p>To present myself in a competent and professional manner to prospective clients and employers.</p>
+			<h2>Audience</h2>
+			<p>The intended audience of this site is for employment reasons, either to give tangible evidence of my work to
 			prospective clients or let employers see the quality/content of my code.</p>
-		<h2>Goal</h2>
-		<p>To attract potential clients and employers to myself and my work.</p>
-		<h1>Persona</h1>
-		<img src="../assets/images/photo.jpg" alt="Image of Jared Mason"/>
-
-
-
+			<h2>Goal</h2>
+			<p>To attract potential clients and employers to myself and my work.</p>
+			<h1>Persona</h1>
+			<img id="jared" src="../assets/images/photo.jpg" alt="Image of Jared Mason"/>
+			<h2 id="about">Jared Mason</h2>
+			<p>Jared is a 32 year old entrepreneur who has started his own photography business. He loves his Nikon D850
+			and has everything he needs to create the best pictures and quality. </p>
+			<p>He has a website, but doesn't think it is relevant to the clients he wants. </p>
+		</main>
 	</body>
 </html>
