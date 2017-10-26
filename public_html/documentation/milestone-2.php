f7629c9efc0fde42eb54fa9dacb37627f36e4dce
Korigan Payne <koriganp@gmail.com>
2017-10-26 10:05:04 -0600

    added meta tag for mobile friendly

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -2,6 +2,7 @@
 <html lang="en-US">
 	<head>
 		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial scale=1, shrink-to-fit=no"/>
 		<meta name="author" content="Korigan Payne"/>
 		<meta name="description" content="direction of the personal website project"/>
 		<link href="../assets/styles/milestone-2.css" rel="stylesheet" type="text/css"/>
