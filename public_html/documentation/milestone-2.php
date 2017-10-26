7b4d52e6dd213cf1988202b9aaa76238b9d0e743
Korigan Payne <koriganp@gmail.com>
2017-10-26 13:57:19 -0600

    changed more stuff

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,10 +13,14 @@
 		<main>
 			<h1><strong>Content Strategy</strong></h1>
 			<h1><strong>Wireframes</strong></h1>
-			<h2>Desktop</h2>
-			<img class="resize" src="../assets/images/pwp.jpg" alt="Wireframe for desktop"/>
-			<h2>Mobile</h2>
-			<img class="resize" src="../assets/images/pwp-mobile.jpg" alt="Wireframe for mobile"/>
+			<div class="column left">
+				<h2>Desktop</h2>
+				<img class="resize" src="../assets/images/pwp.jpg" alt="Wireframe for desktop"/>
+			</div>
+			<div class="column right">
+				<h2>Mobile</h2>
+				<img class="resize" src="../assets/images/pwp-mobile.jpg" alt="Wireframe for mobile"/>
+			</div>
 		</main>
 	</body>
 </html>
