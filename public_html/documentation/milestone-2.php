994b4f2925b7f78f00e798f673af75ff709fa4b6
Korigan Payne <koriganp@gmail.com>
2017-10-26 09:53:04 -0600

    added wireframe diagrams

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,2 +1,19 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="author" content="Korigan Payne"/>
+		<meta name="description" content="direction of the personal website project"/>
+		<link href="../assets/styles/milestone-2.css" rel="stylesheet" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<main>
+			<h1><strong>Content Strategy</strong></h1>
+			<h1><strong>Wireframes</strong></h1>
+			<img class="resize" id="desktop" src="../assets/images/pwp.jpg" alt="Wireframe for desktop"/>
+			<img class="resize" src="../assets/images/pwp-mobile.jpg" alt="Wireframe for mobile"/>
+		</main>
+	</body>
+</html>
