608010d6d4fd6452a43c2110dc1dddfcb56ee209
Korigan Payne <koriganp@gmail.com>
2017-10-29 20:36:10 -0600

    Added content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,6 +12,9 @@
 	<body>
 		<main>
 			<h1><strong>Content Strategy</strong></h1>
+			<p>My professional website is going to be a showcase of some of the skills that I know and am working on developing. I want the site to be very eye catching to promote my ability to make <em>other websites</em> eye catching as well. I want to use three blurred videos in the background layered underneath the main content, spaced evenly between the main content to add action, style, and color. The videos I want to have scroll slower than the page as you go from top to bottom and back up. I will also add spacing bars to hide the edges of the video, overlapping them as the page moves.</p>
+			<p>Between the scrolling bars will be the actual content of the page, including information about me, my skills and experience, and images that I have taken with my Nikon (photoshopped to perfection!) to include my ability to capture just what a potential client is seeking and provide a reason for them to choose me.</p>
+			<p>The footer of the page will have links to my GitHub account, my Linkdin profile, my Twitter account, and my Stack Overflow, as well as a link to a contact page that will have an email link, a contact phone number, and my PO Box.</p>
 			<h1><strong>Wireframes</strong></h1>
 			<div class="container">
 				<div class="left">
