c4cd81e6dbed15e939ba3945b9d9b6ea1cf0aa0a
Korigan Payne <koriganp@gmail.com>
2017-11-11 08:39:54 -0700

    added contact page with links to and from both pages

--- public_html/contact.php
+++ public_html/contact.php
@@ -24,7 +24,7 @@
 			<nav class="navbar navbar-default navbar-expand-md navbar-dark bg-dark text-light rounded" role="navigation">
 				<a class="navbar-brand" href="index.php">Korigan Payne</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span></button>
+					<span class="navbar-toggler-icon"></span></button>
 				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 					<ul class="navbar-nav ml-auto">
 						<li class="nav-item">
