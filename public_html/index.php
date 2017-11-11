666a90f8efee17e41e3eba02da64d08515d27e63
Korigan Payne <koriganp@gmail.com>
2017-11-11 08:31:16 -0700

    started navbar for home page

---
+++ public_html/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<!-- Metadata -->
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, user-scalable = no, initial-scale = 1.0, maximum-scale = 1.0, minimum-scale = 1.0"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<meta title="author" content="Korigan Payne"/>
+		<meta name="description" content="Personal website featuring skills, web-design, and contact information"/>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
+
+		<!-- jQuery, Popper.js, Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<title>Korigan Payne</title>
+	</head>
+	<body>
+		<!-- navigation bar -->
+		<header>
+			<nav class="navbar navbar-default navbar-expand-md navbar-dark bg-dark text-light rounded" role="navigation">
+				<a class="navbar-brand" href="index.php">Korigan Payne</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span></button>
+				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href=""></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href=""></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href=""></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href=""></a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	</body>
+</html>
