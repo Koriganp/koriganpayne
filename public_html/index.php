c030f257f30ed97343317e62c377fefe3e3c67f0
Korigan Payne <koriganp@gmail.com>
2017-11-11 20:46:26 -0700

    added active links in footer, video background for top, and changed page font

--- public_html/index.php
+++ public_html/index.php
@@ -11,6 +11,10 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
 
+		<!-- My CSS -->
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<link href="assets/styles/main.css" rel="stylesheet"/>
+
 		<!-- jQuery, Popper.js, Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
@@ -36,11 +40,14 @@
 				</ul>
 			</div>
 		</nav>
-		<img src=""/>
+		<video class="mb-0 w-100" autoplay loop muted>
+			<source src="assets/videos/hack-code.mp4"/>
+			<p>Your browser doesn't support HTML5 video. Here is a <a href="assets/videos/hack-code.mp4">link</a> to the video</p>
+		</video>
 		<header>
-			<div class="jumbotron bg-secondary">
-				<h1 class="display-1 font-weight-bold text-center">Korigan Payne</h1>
-				<p class="display-4 lead text-center">Web Developer</p>
+			<div class="jumbotron bg-secondary mt-0">
+				<h1 class="display-3 text-center">Korigan Payne</h1>
+				<p class="lead text-center">Web Developer</p>
 			</div>
 		</header>
 		<section class="p-5">
@@ -50,8 +57,8 @@
 						<img class="mw-100 mt-4" src="assets/images/" alt="Image of Korigan Payne"/>
 					</div>
 					<div class="col-md-6 col-lg-9">
-						<p>You can do anything your heart can imagine. Trees grow however makes them happy. Put your feelings into it, your heart, it's your world. I thought today we would make a happy little stream that's just running through the woods here. Only eight colors that you need. Anytime you learn something your time and energy are not wasted. Isn't that fantastic that you can create an almighty tree that fast?</p>
-						<p>In your world you have total and absolute power. That's a son of a gun of a cloud. Nice little clouds playing around in the sky. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen. Paint anything you want on the canvas. Create your own world. We want to use a lot pressure while using no pressure at all.</p>
+						<p class="">You can do anything your heart can imagine. Trees grow however makes them happy. Put your feelings into it, your heart, it's your world. I thought today we would make a happy little stream that's just running through the woods here. Only eight colors that you need. Anytime you learn something your time and energy are not wasted. Isn't that fantastic that you can create an almighty tree that fast?</p>
+						<p class="">In your world you have total and absolute power. That's a son of a gun of a cloud. Nice little clouds playing around in the sky. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen. Paint anything you want on the canvas. Create your own world. We want to use a lot pressure while using no pressure at all.</p>
 					</div>
 				</div>
 			</div>
@@ -69,8 +76,8 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6 col-lg-9">
-						<p>You can do anything your heart can imagine. Trees grow however makes them happy. Put your feelings into it, your heart, it's your world. I thought today we would make a happy little stream that's just running through the woods here. Only eight colors that you need. Anytime you learn something your time and energy are not wasted. Isn't that fantastic that you can create an almighty tree that fast?</p>
-						<p>In your world you have total and absolute power. That's a son of a gun of a cloud. Nice little clouds playing around in the sky. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen. Paint anything you want on the canvas. Create your own world. We want to use a lot pressure while using no pressure at all.</p>
+						<p class="raleway">You can do anything your heart can imagine. Trees grow however makes them happy. Put your feelings into it, your heart, it's your world. I thought today we would make a happy little stream that's just running through the woods here. Only eight colors that you need. Anytime you learn something your time and energy are not wasted. Isn't that fantastic that you can create an almighty tree that fast?</p>
+						<p class="raleway">In your world you have total and absolute power. That's a son of a gun of a cloud. Nice little clouds playing around in the sky. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen. Paint anything you want on the canvas. Create your own world. We want to use a lot pressure while using no pressure at all.</p>
 					</div>
 					<div class="col-md-6 col-lg-3">
 						<img class="mw-100 mt-4" src="assets/images/" alt="Image of"/>
@@ -95,19 +102,19 @@
 			</div>
 		</section>
 		<footer>
-			<div class="bg-dark text-dark nav justify-content-center">
+			<div class="bg-dark text-dark nav justify-content-center pt-3">
 				<ul class="list-inline">
 					<li class="list-inline-item">
-						<a href="#"><img src="assets/images/github.png" alt="Github"/></a>
+						<a href="https://github.com/Koriganp" target="_blank"><img class="w-25 mx-5" src="assets/images/github.png" data-toggle="tooltip" data-placement="top" title="Link to My Github" alt="Github"/></a>
 					</li>
 					<li class="list-inline-item">
-						<a href="#"><img src="assets/images/linkedin.png" alt="LinkedIn"/></a>
+						<a href="https://www.linkedin.com/in/korigan-payne-0570066b" target="_blank"><img class="w-25 mx-5" src="assets/images/linkedin.png" data-toggle="tooltip" data-placement="top" title="Link to My LinkedIn" alt="LinkedIn"/></a>
 					</li>
 					<li class="list-inline-item">
-						<a href="#"><img src="assets/images/stack-overflow.png" alt="Stack OverFlow"/></a>
+						<a href="https://stackoverflow.com/users/8777283/korigan-payne" target="_blank"><img class="w-25 mx-5" src="assets/images/stack-overflow.png" data-toggle="tooltip" data-placement="top" title="Link to My Stack Overflow" alt="Stack OverFlow"/></a>
 					</li>
 					<li class="list-inline-item">
-						<a href="#"><img src="assets/images/twitter.png" alt="Twitter"/></a>
+						<a href="https://twitter.com/KoriganP?lang=en" target="_blank"><img class="w-25 mx-5" src="assets/images/twitter.png" data-toggle="tooltip" data-placement="top" title="Link to My Twitter" alt="Twitter"/></a>
 					</li>
 				</ul>
 			</div>
