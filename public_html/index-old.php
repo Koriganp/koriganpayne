ab8dd73303a5ae991ef7f69a5ce7559f0b3e1a82
Korigan Payne <koriganp@gmail.com>
2017-11-21 07:52:16 -0700

    REBUILD!!!

---
+++ public_html/index-old.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<!-- Metadata -->
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width = device-width, user-scalable = no, initial-scale = 1.0, maximum-scale = 1.0, minimum-scale = 1.0"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<meta name="author" content="Korigan Payne"/>
+		<meta name="description" content="Personal website featuring skills, web-design, and contact information"/>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
+
+		<!-- My CSS -->
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<link href="assets/styles/main.css" rel="stylesheet"/>
+
+		<!-- jQuery, Popper.js, Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<title>Korigan Payne</title>
+	</head>
+	<body class="parallax">
+		<!-- navigation bar -->
+		<nav class="navbar navbar-default navbar-expand-md navbar-dark bg-dark" id="top-of-page">
+			<a class="navbar-brand" href="index.php">Korigan Payne</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a href="https://github.com/Koriganp" target="_blank" class="nav-link"><img class="w-25" src="assets/images/github.png" data-toggle="tooltip" data-placement="bottom" title="My Github" alt="Github"/></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://www.linkedin.com/in/korigan-payne-0570066b" target="_blank" class="nav-link"><img class="w-25" src="assets/images/linkedin.png" data-toggle="tooltip" data-placement="bottom" title="My LinkedIn" alt="LinkedIn"/></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://twitter.com/KoriganP?lang=en" target="_blank" class="nav-link"><img class="w-25" src="assets/images/twitter.png" data-toggle="tooltip" data-placement="bottom" title="My Twitter" alt="Twitter"/></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="index.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="assets/docs/resume.pdf" target="_blank">Resume</a>
+					</li>
+<!--					<li class="nav-item">-->
+<!--						<a class="nav-link" href="portfolio.php">Portfolio</a>-->
+<!--					</li>-->
+					<li class="nav-item">
+						<a class="nav-link" href="contact.php">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="parallax-container bigger">
+			<div class="mt-0 mb-5">
+				<video class="my-0 w-100 video-one" autoplay loop muted>
+					<source src="assets/videos/hack-code.mov"/>
+					<p>Your browser doesn't support HTML5 video. Here is a <a href="assets/videos/hack-code.mov">link</a> to the video</p>
+				</video>
+			</div>
+		</div>
+		<header class="content clearfix">
+			<div class="jumbotron jumbotron-fluid bg-dark m-0">
+				<h1 class="display-3 lime text-center">Korigan Payne</h1>
+				<p class="lead lime text-center">Web Developer</p>
+			</div>
+		</header>
+		<section class="p-5 solid mt-0 lime">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-lg-3">
+						<img class="mw-100" src="assets/images/profile.jpg" alt="Image of Korigan Payne"/>
+					</div>
+					<div class="col-md-6 col-lg-9">
+						<h2>Are you looking for a web developer?</h2>
+						<p>Hello, My name is Korigan Payne! I am an Albuquerque-based freelance web developer and I create beautiful and functional websites for small businesses. If I am not freelancing at some of Albuquerque's finest digital agencies, I am working remotely on projects for clients all over the world. I have a diverse set of skills, ranging from design, to HTML5 + CSS3 + Javascript, all the way to PHP.</p>
+						<p>I primarily use Angular and Bootstrap, but picking up a new framework or language isn't a problem. I am comfortable with developing on the frontend or backend, as well as setting up or managing infrastructure. Each of my designs begin with a blank canvas, which allows your content to guide the layout. I also create with responsiveness in mind, which will make your website accessible across all devices. If you are a business seeking a web presence or an employer looking to hire, you can get in touch with me <a href="contact.php" target="_blank">here</a>.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<div class="my-0">
+			<div class="jumbotron jumbotron-fluid bg-dark mb-0 bigger">
+			</div>
+		</div>
+		<div class="parallax-container">
+			<div class="my-0">
+					<video class="my-0 w-100 video-two" autoplay loop muted>
+					<source src="assets/videos/ethernet.mov"/>
+					<p>Your browser doesn't support HTML5 video. Here is a <a href="assets/videos/ethernet.mov">link</a> to the video</p>
+				</video>
+			</div>
+		</div>
+		<div class="my-0">
+			<div class="jumbotron jumbotron-fluid bg-dark mb-0 bigger">
+			</div>
+		</div>
+		<section class="p-5 solid mt-0 lime clearfix">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12">
+						<h2 class="lime text-center">Let me work for you!</h2>
+						<p>If you need custom images for your website showcasing features, people, or a beautiful shot of your physical location, not only am I a web developer, but I am also a photographer. I do things differently than other photographers might. Beyond simply taking a nice picture, I take a step back to observe and capture all the emotion and unique details that will make the images on <em>your</em> site</p>
+						<p class="lime display-4 text-center">STAND OUT</p>
+						<p class="text-center">from other sites in a similar market.</p>
+						<p>I have over five years experience working with Adobe Photoshop, Lightroom, and Illustrator which will ensure the quality of your custom image and the ability to transform an ordinary image into an extraordinary piece of art. I only use professional equipment and this benefit is included with the design of your website when you <a href="contact.php" target="_blank">hire me!</a></p>
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+							<img class="mw-100 mt-4" src="assets/images/snake.jpg" alt="Image of Rattlesnake"/>
+						</div>
+						<div class="col-md-6">
+							<img class="mw-100 mt-4" src="assets/images/aspen.jpg" alt="Image of Aspen Trees"/>
+						</div>
+						<div class="col-md-6">
+							<img class="mw-100 mt-4" src="assets/images/cactus.jpg" alt="Image of Cactus"/>
+						</div>
+						<div class="col-md-6">
+							<img class="mw-100 mt-4" src="assets/images/bastian.jpg" alt="Image of my son and father-in-law"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="mt-0">
+			<div class="bg-dark text-light nav justify-content-center p-5">
+				<p>2017 Korigan Payne</p>
+			</div>
+		</footer>
+	</body>
+</html>
