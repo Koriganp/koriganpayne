3cd56ca3417eaa695f8cb7b345974a06c155cdd4
Korigan Payne <koriganp@gmail.com>
2017-11-21 12:37:25 -0700

    cleaned up code and addressed some small errors

--- public_html/index.php
+++ public_html/index.php
@@ -1,26 +1,33 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
+
 		<!-- Metadata -->
+
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width = device-width, user-scalable = no, initial-scale = 1.0, maximum-scale = 1.0, minimum-scale = 1.0"/>
 		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
 		<meta name="author" content="Korigan Payne"/>
-		<meta name="description" content="Personal website featuring skills, web-design, and contact information"/>
+		<meta name="description" content="Personal website featuring skills, web-design, and contact information for web development in Albuquerque."/>
 
 		<!-- Bootstrap CSS -->
+
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
 
 		<!-- My CSS -->
+
 		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
 		<link href="assets/styles/main.css" rel="stylesheet"/>
 
 		<!-- jQuery, Popper.js, Bootstrap JS -->
+
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
-		<title>Korigan Payne</title>
+		<!-- Page Title -->
+
+		<title>Korigan Payne Web Developer</title>
 	</head>
 	<body>
 
