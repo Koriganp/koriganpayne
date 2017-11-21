72a3f82a15fc4496bd97700c964a479df09193e6
Korigan Payne <koriganp@gmail.com>
2017-11-21 09:32:00 -0700

    Remade the home page

--- public_html/index.php
+++ public_html/index.php
@@ -26,5 +26,118 @@
 
 		<!-- Navigation Bar -->
 
+		<nav class="navbar navbar-default navbar-expand-md navbar-light bg-light">
+			<a class="navbar-brand" href="index.php">KoriganPayne.com</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a href="https://github.com/Koriganp" target="_blank" class="nav-link tab-size"><img  class="w-50" src="assets/images/github.png" data-toggle="tooltip" data-placement="bottom" title="My Github" alt="Github"/></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://www.linkedin.com/in/korigan-payne-0570066b" target="_blank" class="nav-link tab-size"><img  class="w-50" src="assets/images/linkedin.png" data-toggle="tooltip" data-placement="bottom" title="My LinkedIn" alt="LinkedIn"/></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://twitter.com/KoriganP?lang=en" target="_blank" class="nav-link tab-size"><img  class="w-50" src="assets/images/twitter.png" data-toggle="tooltip" data-placement="bottom" title="My Twitter" alt="Twitter"/></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="index.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="assets/docs/resume.pdf" target="_blank">Resume</a>
+					</li>
+					<!--					<li class="nav-item">-->
+					<!--						<a class="nav-link" href="portfolio.php">Portfolio</a>-->
+					<!--					</li>-->
+					<li class="nav-item">
+						<a class="nav-link" href="contact.php">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<!-- Parallax -->
+
+		<div class="parallax-content">
+			<div class="parallax">
+			</div>
+		</div>
+
+		<!-- Main box -->
+
+		<div class="jumbotron jumbotron-fluid bg-light m-0">
+			<h1 class="display-3 text-center">Korigan Payne</h1>
+			<p class="lead text-center">Web Developer</p>
+		</div>
+
+		<!-- About content -->
+
+		<section class="p-5 solid mt-0 text-light">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-lg-3">
+						<img class="mw-100" src="assets/images/profile.jpg" alt="Image of Korigan Payne"/>
+					</div>
+					<div class="col-md-6 col-lg-9">
+						<h2>Are you looking for a web developer?</h2>
+						<p>Hello, My name is Korigan Payne! I am an Albuquerque-based freelance web developer and I create beautiful and functional websites for small businesses. If I am not freelancing at some of Albuquerque's finest digital agencies, I am working remotely on projects for clients all over the world. I have a diverse set of skills, ranging from design, to HTML5 + CSS3 + Javascript, all the way to PHP.</p>
+						<p>I primarily use Angular and Bootstrap, but picking up a new framework or language isn't a problem. I am comfortable with developing on the frontend or backend, as well as setting up or managing infrastructure. Each of my designs begin with a blank canvas, which allows your content to guide the layout. I also create with responsiveness in mind, which will make your website accessible across all devices. If you are a business seeking a web presence or an employer looking to hire, you can get in touch with me <a href="contact.php" target="_blank">here</a>.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Parallax 2 -->
+		<div class="parallax-content parallax-content-2">
+			<div class="parallax">
+			</div>
+		</div>
+
+		<!-- Middle breaking bar -->
+		<div class="jumbotron jumbotron-fluid bg-light m-0 bigger">
+		</div>
+
+		<!-- Parallax 3 -->
+		<div class="parallax-content parallax-content-3">
+			<div class="parallax">
+			</div>
+		</div>
+
+		<!-- End content -->
+		<section class="p-5 solid mt-0 text-light">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12">
+						<h2 class="text-center">Let me work for you!</h2>
+						<p>If you need custom images for your website showcasing features, people, or a beautiful shot of your physical location, not only am I a web developer, but I am also a photographer. I do things differently than other photographers might. Beyond simply taking a nice picture, I take a step back to observe and capture all the emotion and unique details that will make the images on <em>your</em> site</p>
+						<p class="display-4 text-center">STAND OUT</p>
+						<p class="text-center">from other sites in a similar market.</p>
+						<p>I have over five years experience working with Adobe Photoshop, Lightroom, and Illustrator which will ensure the quality of your custom image and the ability to transform an ordinary image into an extraordinary piece of art. I only use professional equipment and this benefit is included with the design of your website when you <a href="contact.php" target="_blank">hire me!</a></p>
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+							<img class="mw-100 mt-4" src="assets/images/snake.jpg" alt="Image of Rattlesnake"/>
+						</div>
+						<div class="col-md-6">
+							<img class="mw-100 mt-4" src="assets/images/aspen.jpg" alt="Image of Aspen Trees"/>
+						</div>
+						<div class="col-md-6">
+							<img class="mw-100 mt-4" src="assets/images/cactus.jpg" alt="Image of Cactus"/>
+						</div>
+						<div class="col-md-6">
+							<img class="mw-100 mt-4" src="assets/images/bastian.jpg" alt="Image of my son and father-in-law"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="mt-0">
+			<div class="bg-light justify-content-center p-5">
+				<p>2017 Korigan Payne</p>
+			</div>
+		</footer>
+
 	</body>
 </html>
