59197b20f84412653d0f54b251070e1e1fb5bee4
Korigan Payne <koriganp@gmail.com>
2017-11-21 16:32:08 -0700

    NICE!!!

--- public_html/contact.php
+++ public_html/contact.php
@@ -1,32 +1,40 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
+
 		<!-- Metadata -->
+
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width = device-width, user-scalable = no, initial-scale = 1.0, maximum-scale = 1.0, minimum-scale = 1.0"/>
 		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
 		<meta name="author" content="Korigan Payne"/>
-		<meta name="description" content="Personal website featuring skills, web-design, and contact information"/>
+		<meta name="description" content="Personal website featuring skills, web-design, and contact information for web development in Albuquerque."/>
 
 		<!-- Bootstrap CSS -->
+
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
 
 		<!-- My CSS -->
+
 		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
 		<link href="assets/styles/main.css" rel="stylesheet"/>
 
 		<!-- jQuery, Popper.js, Bootstrap JS -->
+
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
-		<title>Korigan Payne</title>
+		<!-- Page Title -->
+
+		<title>Korigan Payne Web Developer</title>
 	</head>
 	<body>
 
 		<!-- Navigation Bar -->
 
-		<nav class="navbar navbar-default navbar-expand-md navbar-light bg-light">
+		<nav id="up" class="navbar navbar-default navbar-expand-md navbar-light bg-light">
 			<a class="navbar-brand" href="index.php">KoriganPayne.com</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -34,13 +42,13 @@
 			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
-						<a href="https://github.com/Koriganp" target="_blank" class="nav-link tab-size"><img  class="w-50" src="assets/images/github.png" data-toggle="tooltip" data-placement="bottom" title="My Github" alt="Github"/></a>
+						<a href="https://github.com/Koriganp" target="_blank" class="nav-link"><i class="fa fa-github fa-2x"></i></a>
 					</li>
 					<li class="nav-item">
-						<a href="https://www.linkedin.com/in/korigan-payne-0570066b" target="_blank" class="nav-link tab-size"><img  class="w-50" src="assets/images/linkedin.png" data-toggle="tooltip" data-placement="bottom" title="My LinkedIn" alt="LinkedIn"/></a>
+						<a href="https://www.linkedin.com/in/korigan-payne-0570066b" target="_blank" class="nav-link"><i class="fa fa-linkedin fa-2x"></i></a>
 					</li>
 					<li class="nav-item">
-						<a href="https://twitter.com/KoriganP?lang=en" target="_blank" class="nav-link tab-size"><img  class="w-50" src="assets/images/twitter.png" data-toggle="tooltip" data-placement="bottom" title="My Twitter" alt="Twitter"/></a>
+						<a href="https://twitter.com/KoriganP?lang=en" target="_blank" class="nav-link"><i class="fa fa-twitter fa-2x"></i></a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="index.php">Home</a>
@@ -57,6 +65,9 @@
 				</ul>
 			</div>
 		</nav>
+
+		<!-- Footer -->
+
 		<footer>
 			<div class="bg-light text-dark nav justify-content-center pt-3">
 				<p>2017 Korigan Payne</p>
