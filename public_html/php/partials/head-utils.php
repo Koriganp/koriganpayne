42189d27c2ec07c35db3834e1dffdc4d4b7ad695
Korigan Payne <koriganp@gmail.com>
2017-11-22 13:39:31 -0700

    made head utilities

---
+++ public_html/php/partials/head-utils.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Get the relative path.
+ * @see https://raw.githubusercontent.com/kingscreations/farm-to-you/master/php/lib/_header.php FarmToYou Header
+ **/
+// include the appropriate number of dirname() functions
+// on line 8 to correctly resolve your directory's path
+require_once(dirname(dirname(__DIR__)) . "/root-path.php");
+$CURRENT_DEPTH = substr_count($CURRENT_DIR, "/");
+$ROOT_DEPTH = substr_count($ROOT_PATH, "/");
+$DEPTH_DIFFERENCE = $CURRENT_DEPTH - $ROOT_DEPTH;
+$PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+
+		<!-- Metadata -->
+
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width = device-width, user-scalable = no, initial-scale = 1.0, maximum-scale = 1.0, minimum-scale = 1.0"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<meta name="author" content="Korigan Payne"/>
+		<meta name="description" content="Personal website featuring skills, web-design, and contact information for web development in Albuquerque."/>
+
+		<!-- Bootstrap CSS -->
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
+
+		<!-- My CSS -->
+
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<link rel="stylesheet" href="<?php echo $PREFIX;?>../font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo $PREFIX;?>../node_modules/animate.css/animate.min.css">
+		<link href="<?php echo $PREFIX;?>styles/main.css" rel="stylesheet"/>
+
+		<!-- jQuery, Popper.js, Bootstrap JS -->
+
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src="https://www.google.com/recaptcha/api.js"></script>
+
+		<!-- JavaScript Form Validator File -->
+		<script src="<?php echo $PREFIX;?>js/form-validate.js" type="text/javascript"></script>
+
+		<!-- Page Title -->
+
+		<!-- echo the $PAGE_TITLE variable here -->
+		<title><?php echo $PAGE_TITLE;?></title>
+	</head>
