a0b3a7552668cdf2588e8bb9bc34bb01a89d5ae1
Korigan Payne <koriganp@gmail.com>
2017-11-24 11:51:00 -0700

    added animations to columns

--- public_html/portfolio/index.php
+++ public_html/portfolio/index.php
@@ -18,11 +18,13 @@ require_once("../php/partials/head-utils.php");
 
 				<!-- This is the first demo website -->
 
-				<div class="row blank mt-5">
-					<div  id="law" class="col-md-8 my-5 pl-5 animated">
-						<a href="sample-sites/law-site/index.html" target="_blank"><img src="<?php echo $PREFIX;?>images/law-firm.jpg" alt="Law Firm Site"/></a>
+				<div class="container">
+					<div class="row blank my-5">
+						<div  id="law" class="col-md-8 my-5 animated">
+							<a href="sample-sites/law-site/index.html" target="_blank"><img src="<?php echo $PREFIX;?>images/law-firm-site.jpg" alt="Law Firm Site"/></a>
+						</div>
+						<p id="lawText" class="col-md-4 text-dark my-5 pl-5 animated">This is a sample multi-page website for a law firm. You can view it <a href="sample-sites/law-site/index.html" target="_blank">here</a>, or you can just click on the image. If you like what you see, please <a href="<?php echo $PREFIX;?>contact/index.php">contact me</a> for a quote.</p>
 					</div>
-					<p id="lawText" class="col-md-4 text-dark my-5 animated">This is a sample website for law firm. You can view it <a href="sample-sites/law-site/index.html" target="_blank">here</a>, or you can just click on the image. If you like what you see, please <a href="<?php echo $PREFIX;?>contact/index.php">contact me</a> for a quote.</p>
 				</div>
 			</div>
 		</section>
@@ -34,16 +36,16 @@ require_once("../php/partials/head-utils.php");
 
 				<!-- This is the second demo website -->
 
-				<div class="row blank mt-5">
-					<p id="landscapeText" class="col-md-4 text-dark my-5 animated">This is a sample website for a landscaping company. It is just a demo and not an actual site. You can view it <a href="sample-sites/" target="_blank">here</a>, or you can just click on the image. If you like what you see, please fill out the form on my <a href="<?php echo $PREFIX;?>contact/index.php">contact page</a>and I will get back to you with more info.</p>
-				</div>
-					<div  id="landscape" class="col-md-8 my-5 pl-5 animated">
-						<a href="sample-sites/" target="_blank"><img src="<?php echo $PREFIX;?>images/" alt="Landscaping Site"/></a>
+				<div class="container">
+					<div class="row blank my-5">
+						<p id="landscapeText" class="col-md-4 text-dark my-5 pr-1 animated">This is a sample static website for a landscaping company. It is just a demo and not an actual site. You can view it <a href="sample-sites/" target="_blank">here</a>, or you can just click on the image. If you like what you see, please fill out the form on my <a href="<?php echo $PREFIX;?>contact/index.php">contact page</a> and I will get back to you with more info.</p>
+						<div  id="landscape" class="col-md-8 my-5 animated">
+							<a href="sample-sites/" target="_blank"><img src="<?php echo $PREFIX;?>images/landscape-site.jpg" alt="Landscaping Site"/></a>
+						</div>
 					</div>
 				</div>
+			</div>
 		</section>
-
-
 
 		<!-- Parallax 3 -->
 
@@ -52,11 +54,13 @@ require_once("../php/partials/head-utils.php");
 
 				<!-- This is the third demo website -->
 
-				<div class="row blank mt-5">
-					<div  id="restaurant" class="col-md-8 my-5 pl-5 animated">
-						<a href="sample-sites/" target="_blank"><img src="<?php echo $PREFIX;?>images/" alt="Restaurant Site"/></a>
+				<div class="container">
+					<div class="row blank my-5">
+						<div  id="restaurant" class="col-md-8 my-5 animated">
+							<a href="sample-sites/" target="_blank"><img src="<?php echo $PREFIX;?>images/restaurant-site.jpg" alt="Restaurant Site"/></a>
+						</div>
+						<p id="restaurantText" class="col-md-4 text-light my-5 pl-5 animated">This is a sample website for a restaurant. It is also not a real site, just a template to show you what your potential site could look like. You can view it <a href="sample-sites/" target="_blank">here</a>, or you can just click on the image. If you like what you see, please <a href="<?php echo $PREFIX;?>contact/index.php">contact me</a>.</p>
 					</div>
-					<p id="restaurantText" class="col-md-4 text-dark my-5 animated">This is a sample website for a restaurant. It is also not a real site, just a template to show you what your potential site could look like. You can view it <a href="sample-sites/" target="_blank">here</a>, or you can just click on the image. If you like what you see, please <a href="<?php echo $PREFIX;?>contact/index.php">contact me</a>.</p>
 				</div>
 			</div>
 		</section>
