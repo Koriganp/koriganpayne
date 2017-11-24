0e15af9e914a7b56372bf25760dd6f7bd29892c7
Korigan Payne <koriganp@gmail.com>
2017-11-24 12:42:18 -0700

    Cleaned up js file

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	// Activate heading animations on scroll
 	$('#web').waypoint(function() {
 		$('#web').addClass('zoomIn');
 	}, {offset: '80%'});
@@ -12,6 +13,20 @@ $(document).ready(function() {
 		$('#stand').addClass('bounceInDown');
 	}, {offset: '95%'});
 
+	// hide elements on page load
+	$('#law').css('opacity', 0);
+
+	$('#lawText').css('opacity', 0);
+
+	$('#landscapeText').css('opacity', 0);
+
+	$('#landscape').css('opacity', 0);
+
+	$('#restaurant').css('opacity', 0);
+
+	$('#restaurantText').css('opacity', 0);
+
+	//Animate objects as they appear on screen
 	$('#law').waypoint(function() {
 		$('#law').addClass('fadeInLeft');
 	}, {offset: '80%'});
