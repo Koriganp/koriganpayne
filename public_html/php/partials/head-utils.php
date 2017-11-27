f4db698cf8b6d8df354852d83dfaddb1ec5e3483
Korigan Payne <koriganp@gmail.com>
2017-11-27 15:54:13 -0700

    fixed so it works on live site

--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -34,11 +34,11 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 
 		<!-- Font Awesome -->
 
-		<link rel="stylesheet" href="<?php echo $PREFIX;?>../font-awesome/css/font-awesome.min.css"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
 		<!-- Animate.css -->
 
-		<link rel="stylesheet" href="<?php echo $PREFIX;?>../node_modules/animate.css/animate.min.css"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
 
 		<!-- My CSS -->
 
